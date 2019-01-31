1ec72e942a3269c882121b4fef62712e54d8cabd
gaochao <gaochao5kuba@163,com>
2019-01-31 17:31:28 +0800

    提交m的依赖项

---
+++ src/main/java/com/qbgc/m/base/QbgcMBaseApplication.java
@@ -0,0 +1,14 @@
+package com.qbgc.m.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbgcMBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbgcMBaseApplication.class, args);
+	}
+
+}
+
